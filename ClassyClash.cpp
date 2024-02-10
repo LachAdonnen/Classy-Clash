8a30052b42eb74d907f23bbd2848d3b387cc0e24
Alex McClain <alex.mcclain423@gmail.com>
2024-02-10 08:10:14 -0600

    Draw world map background

--- ClassyClash.cpp
+++ ClassyClash.cpp
@@ -8,16 +8,21 @@ int main()
     InitWindow(screenWidth, screenHeight, "Classy Clash");
     SetTargetFPS(60);
 
+    Texture2D background = LoadTexture("nature_tileset/World Map.png");
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(WHITE);
 
         // Draw your game here
+        Vector2 mapPosition = {0.0f, 0.0f};
+        DrawTextureEx(background, mapPosition, 0, 4, WHITE);
 
         EndDrawing();
     }
 
+    UnloadTexture(background);
     CloseWindow();
 
     return 0;
