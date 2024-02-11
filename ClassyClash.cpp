5f5d0f69f18f283975beb07c35228a01bdbcb74e
Alex McClain <alex.mcclain423@gmail.com>
2024-02-11 07:50:19 -0600

    Move the screen in response to user input

--- ClassyClash.cpp
+++ ClassyClash.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "raymath.h"
 
 int main()
 {
@@ -9,14 +10,25 @@ int main()
     SetTargetFPS(60);
 
     Texture2D background = LoadTexture("nature_tileset/World Map.png");
+    Vector2 mapPosition = {0.0f, 0.0f};
+    float moveSpeed = 4.0f;
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(WHITE);
 
+        Vector2 direction = {0.0f, 0.0f};
+        if (IsKeyDown(KEY_A)) direction.x -= 1.0;
+        if (IsKeyDown(KEY_D)) direction.x += 1.0;
+        if (IsKeyDown(KEY_W)) direction.y -= 1.0;
+        if (IsKeyDown(KEY_S)) direction.y += 1.0;
+        if (Vector2Length(direction) != 0) 
+        {
+            mapPosition = Vector2Subtract(mapPosition, Vector2Scale(Vector2Normalize(direction), moveSpeed));
+        }
+
         // Draw your game here
-        Vector2 mapPosition = {0.0f, 0.0f};
         DrawTextureEx(background, mapPosition, 0, 4, WHITE);
 
         EndDrawing();
