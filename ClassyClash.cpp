68a5d98ddf271e4f502ed34b47ad64e8e0381e68
Alex McClain <alex.mcclain423@gmail.com>
2024-02-11 10:10:37 -0600

    Refactor player code into Character class

--- ClassyClash.cpp
+++ ClassyClash.cpp
@@ -1,6 +1,83 @@
 #include "raylib.h"
 #include "raymath.h"
 
+class Character
+{
+public:
+    Vector2 getWorldPosition() { return worldPosition; }
+    void setScreenPosition(int screenWidth, int screenHeight);
+    void tick(float dT);
+
+private:
+    Texture2D textureIdle{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D textureRun{LoadTexture("characters/knight_run_spritesheet.png")};
+    Texture2D texture{textureIdle};
+    int spriteMaxFrames{6};
+    float spriteWidth{textureIdle.width / (float)spriteMaxFrames};
+    float spriteScale{4.f};
+
+    Vector2 screenPosition{};
+    Vector2 worldPosition{};
+    const float moveSpeed{4.f};
+
+    float rightLeft{1.f};
+    int spriteCurrentFrame{0};
+    float spriteRunningTime{0.f};
+    const float spriteUpdateTime{1.f / 12.f};
+};
+
+void Character::setScreenPosition(int screenWidth, int screenHeight)
+{
+    screenPosition =
+        {
+            (screenWidth / 2.f) - (spriteScale * (texture.width / spriteMaxFrames) / 2.f),
+            (screenHeight / 2.f) - (spriteScale * (texture.height) / 2.f)};
+}
+
+void Character::tick(float dT)
+{
+    Vector2 direction = {0.0f, 0.0f};
+    if (IsKeyDown(KEY_A))
+        direction.x -= 1.0;
+    if (IsKeyDown(KEY_D))
+        direction.x += 1.0;
+    if (IsKeyDown(KEY_W))
+        direction.y -= 1.0;
+    if (IsKeyDown(KEY_S))
+        direction.y += 1.0;
+    if (Vector2Length(direction) != 0)
+    {
+        worldPosition = Vector2Add(worldPosition, Vector2Scale(Vector2Normalize(direction), moveSpeed));
+        rightLeft = direction.x >= 0 ? 1.f : -1.f;
+        texture = textureRun;
+    }
+    else
+        texture = textureIdle;
+
+    spriteRunningTime += dT;
+    if (spriteRunningTime >= spriteUpdateTime)
+    {
+        spriteRunningTime = 0.0f;
+        spriteCurrentFrame++;
+        if (spriteCurrentFrame >= spriteMaxFrames)
+            spriteCurrentFrame = 0;
+    }
+
+    DrawTexturePro(
+        texture,
+        {spriteCurrentFrame * spriteWidth,
+         0,
+         rightLeft * spriteWidth,
+         (float)texture.height},
+        {screenPosition.x,
+         screenPosition.y,
+         spriteScale * spriteWidth,
+         spriteScale * (float)texture.height},
+        {0, 0},
+        spriteScale,
+        WHITE);
+}
+
 int main()
 {
     const int screenWidth = 384;
@@ -11,72 +88,20 @@ int main()
 
     Texture2D background = LoadTexture("nature_tileset/World Map.png");
     Vector2 mapPosition = {0.0f, 0.0f};
-    float moveSpeed = 4.0f;
 
-    Texture2D playerTextureIdle = LoadTexture("characters/knight_idle_spritesheet.png");
-    Texture2D playerTextureRun = LoadTexture("characters/knight_run_spritesheet.png");
-    Texture2D playerTexture = playerTextureIdle;
-    const int playerSpriteCount = 6;
-    const float playerSpriteWidth = (float)(playerTexture.width / playerSpriteCount);
-    float playerSpriteScale = 4.0f;
-    Vector2 playerPosition = {
-        screenWidth / 2.0f - (playerSpriteScale * 0.5f * playerSpriteWidth),
-        screenHeight / 2.0f - (playerSpriteScale * 0.5f * ((float)playerTexture.height))
-    };
-    float playerRightLeft = 1.0f; // Positive for right, negative for left
-    int playerSpriteCurrentFrame = 0;
-    float playerSpriteRunningTime = 0.0f;
-    const float playerSpriteUpdateTime = 1.0f / 12.0f;
+    Character player{};
+    player.setScreenPosition(screenWidth, screenHeight);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(WHITE);
 
-        const float dT = GetFrameTime();
-
-        Vector2 direction = {0.0f, 0.0f};
-        if (IsKeyDown(KEY_A)) direction.x -= 1.0;
-        if (IsKeyDown(KEY_D)) direction.x += 1.0;
-        if (IsKeyDown(KEY_W)) direction.y -= 1.0;
-        if (IsKeyDown(KEY_S)) direction.y += 1.0;
-        if (Vector2Length(direction) != 0) 
-        {
-            mapPosition = Vector2Subtract(mapPosition, Vector2Scale(Vector2Normalize(direction), moveSpeed));
-            playerRightLeft = direction.x >= 0 ? 1.0f : -1.0f;
-            playerTexture = playerTextureRun;
-        }
-        else playerTexture = playerTextureIdle;
-
         // Draw your game here
+        mapPosition = Vector2Scale(player.getWorldPosition(), -1.f);
         DrawTextureEx(background, mapPosition, 0, 4, WHITE);
 
-        playerSpriteRunningTime += dT;
-        if (playerSpriteRunningTime >= playerSpriteUpdateTime)
-        {
-            playerSpriteRunningTime = 0.0f;
-            playerSpriteCurrentFrame++;
-            if (playerSpriteCurrentFrame >= playerSpriteCount) playerSpriteCurrentFrame = 0;
-        }
-
-        DrawTexturePro(
-            playerTexture, 
-            {
-                playerSpriteCurrentFrame * playerSpriteWidth, 
-                0, 
-                playerRightLeft * playerSpriteWidth, 
-                (float)playerTexture.height
-            },
-            {
-                playerPosition.x, 
-                playerPosition.y, 
-                playerSpriteScale * playerSpriteWidth, 
-                playerSpriteScale * (float)playerTexture.height
-            },
-            {0, 0}, 
-            playerSpriteScale, 
-            WHITE
-        );
+        player.tick(GetFrameTime());
 
         EndDrawing();
     }
