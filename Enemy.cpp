e448ff740b2a472957dca2670f3f7f3a89f9056a
Alex McClain <alex.mcclain423@gmail.com>
2024-02-12 10:19:09 -0600

    Add basic chasing AI

--- Enemy.cpp
+++ Enemy.cpp
@@ -10,10 +10,13 @@ Enemy::Enemy(Vector2 worldPosition, Texture2D textureIdle, Texture2D textureRun)
     texture = textureIdle;
     spriteWidth = static_cast<float>(texture.width) / spriteMaxFrames;
     spriteHeight = static_cast<float>(texture.height);
+    moveSpeed = 3.5f;
 }
 
 void Enemy::tick(float dT)
 {
+    Vector2 toTargetDirection = Vector2Normalize(Vector2Subtract(target->getScreenPosition(), screenPosition));
+    worldPosition = Vector2Add(worldPosition, Vector2Scale(toTargetDirection, moveSpeed));
     screenPosition = Vector2Subtract(worldPosition, target->getWorldPosition());
     BaseCharacter::tick(dT);
 }
